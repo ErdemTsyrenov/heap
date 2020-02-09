3548ea963f99f3fae7b806f92da158048eba3b17
ErdemTsyrenov <46107862+ErdemTsyrenov@users.noreply.github.com>
2020-02-09 11:29:22 +0300

    Created xcode project

---
+++ Heap/Heap/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Heap
+//
+//  Created by Эрдэм Цыренов on 09.02.2020.
+//  Copyright © 2020 Эрдэм Цыренов. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
